336bd348abb6a8d3b02c53d8758e0ddbbeabcfc0
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2026-07-29 11:27:59 +0900

    Remove unused BUF_reverse()
    BUF_reverse() has no callers and is not part of the public API. Remove the
    unused function, which also eliminates its -Wmissing-prototypes warning.

--- src/lib/libcrypto/buffer/buffer.c
+++ src/lib/libcrypto/buffer/buffer.c
@@ -133,24 +133,3 @@ BUF_MEM_grow_clean(BUF_MEM *str, size_t len)
 	return (len);
 }
 LCRYPTO_ALIAS(BUF_MEM_grow_clean);
-
-void
-BUF_reverse(unsigned char *out, const unsigned char *in, size_t size)
-{
-	size_t i;
-
-	if (in) {
-		out += size - 1;
-		for (i = 0; i < size; i++)
-			*out-- = *in++;
-	} else {
-		unsigned char *q;
-		char c;
-		q = out + size - 1;
-		for (i = 0; i < size / 2; i++) {
-			c = *q;
-			*q-- = *out;
-			*out++ = c;
-		}
-	}
-}
